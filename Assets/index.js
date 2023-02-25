0823f3942d2fb9e7aa538442e25f1c2c0ef85abe
Siraphop Sukchu <soraphopsukchu@gmail.com>
2023-02-25 11:44:26 +0700

    Add file

---
+++ Assets/index.js
@@ -0,0 +1,12 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+  
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+  behavior: 'smooth'
+}
