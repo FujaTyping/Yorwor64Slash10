1228247fecf9ddf20f378c2da7be5002604f23c3
Siraphop Sukchu <soraphopsukchu@gmail.com>
2023-03-15 00:12:38 +0700

    Add file (not finish)

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 function NotOP () {
     swal("❌ ผิดผลาด", "ตอนนี้ระเบียนผลการเรียนยังไม่เปิดนะ", "error");
 };
+
+document.onreadystatechange = function () {
+    window.scrollTo(0, 0);
+}
+
+window.beforeunload = function () {
+    window.scrollTo(0, 0);
+}
