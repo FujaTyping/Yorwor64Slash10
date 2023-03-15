2c561a3364bd1d409434caedd4af03bdf30b4ff2
Siraphop Sukchu <soraphopsukchu@gmail.com>
2023-03-15 11:02:37 +0700

    Add file (finish)

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ function NotOP () {
     swal("❌ ผิดผลาด", "ตอนนี้ระเบียนผลการเรียนยังไม่เปิดนะ", "error");
 };
 
+function ClassWhite () {
+    swal("❌ ผิดผลาด", "ยังไม่มีข้อมูลห้องเรียนสีข้าวเลย", "error");
+};
+
+function NoticeLog () {
+    swal("ℹ ข้อมูล", "ล็อกอินสำหรับผู้ดูและระบบเท่านั้น", "info");
+};
+
 document.onreadystatechange = function () {
     window.scrollTo(0, 0);
 }
