3536f7382efd34018076729a3b185cf1bd10ac3d
Siraphop Sukchu <soraphopsukchu@gmail.com>
2023-05-02 13:59:14 +0700

    Add Update and Remove file

--- real/disable.js
+++
@@ -1,7 +0,0 @@
-var message = "Disabled";
-    
-function rtclickcheck(keyp){ if (navigator.appName == "Netscape" && keyp.which == 3){ alert(message); return false; }
-
-if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { alert(message); return false; } }
-
-document.onmousedown = rtclickcheck;
--- real/loader.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    const Load = document.getElementById("loader")
-    const App = document.getElementById("app")
-    const Bg = document.getElementById("bg")
-    
-    Load.style.display = "none";
-    Bg.style.display = "block";
-};
