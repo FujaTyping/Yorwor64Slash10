99c5e965caa8a0c5ebad6feb06a6c4b58462226d
Siraphop Sukchu <soraphopsukchu@gmail.com>
2023-05-02 09:00:40 +0700

    Add and Update file

---
+++ real/disable.js
@@ -0,0 +1,7 @@
+var message = "Disabled";
+    
+function rtclickcheck(keyp){ if (navigator.appName == "Netscape" && keyp.which == 3){ alert(message); return false; }
+
+if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { alert(message); return false; } }
+
+document.onmousedown = rtclickcheck;
---
+++ real/loader.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    const Load = document.getElementById("loader")
+    const App = document.getElementById("app")
+    const Bg = document.getElementById("bg")
+    
+    Load.style.display = "none";
+    Bg.style.display = "block";
+};
