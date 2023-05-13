775d8f3007b918cd74a95650a662949bdcb99e45
Siraphop Sukchu <soraphopsukchu@gmail.com>
2023-05-13 13:35:15 +0700

    Update file

--- loader.js
+++ loader.js
@@ -11,11 +11,11 @@ window.onload = function() {
     console.log(page)
     if (page == "" || page == "index.html" || page == "index") {
         Swal.fire({
-            title: 'ตารางกิจกรรม',
-            text: 'สามารถกดค้างแล้วกดเปิดรูปภาพได้',
-            imageUrl: 'https://cdn.discordapp.com/attachments/1024635780360056883/1085491294668861470/103776.jpg',
-            imageWidth: 320,
-            imageHeight: 420,
+            title: 'ประกาศจากโรงเรียน',
+            text: 'ประกาศ ขอความร่วมมือผู้ใช้อีเมล @hatyaiwit.ac.th ดำเนินการจัดสรรพื้นที่เก็บข้อมูล บน Google Workspace for Education ดังนี้',
+            imageUrl: 'https://cdn.discordapp.com/attachments/1015944183221063700/1106546289321185371/346476708_1639215433265235_2711160482045570969_n.png',
+            imageWidth: 300,
+            imageHeight: 200,
             confirmButtonText: 'โอเค ฉันเข้าใจแล้ว !'
         })
     }
