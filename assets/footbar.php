51d39f961fde23cb1bfff9c8bc9ab9a2b26883cd
Siraphop Sukchu <86290693+FujaTyping@users.noreply.github.com>
2023-06-30 17:43:51 +0700

    Rework entire footer

--- assets/footbar.php
+++ assets/footbar.php
@@ -2,32 +2,37 @@
 
 <br>
 <div class="d-flex justify-content-center">
-    <button onclick="document.body.scrollTop = 0; document.documentElement.scrollTop = 0;"
-        style="background-color: rgb(45, 118, 255);color: white;" type="button" class="btn">⇧ กลับไปด้านบน</button>
+        <button onclick="document.body.scrollTop = 0; document.documentElement.scrollTop = 0;"
+                style="background-color: rgb(45, 118, 255);color: white;" type="button" class="btn">⇧
+                กลับไปด้านบน</button>
 </div>
 <footer style="padding: 20px;" class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <p class="col-md-4 mb-0 text-body-secondary">© 2022-2023 Siraphop Sukchu</p>
-    <a href="/"
-        class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-        <svg class="bi me-2" width="40" height="32">
-            <use xlink:href="#bootstrap"></use>
-        </svg>
-    </a>
-    <ul class="nav col-md-4 justify-content-end">
-        <li class="nav-item"><a href="https://yorworplatform.com" class="nav-link px-2 text-body-secondary">Yorwor
-                Platform</a></li>
-        <li class="nav-item"><a href="https://sites.google.com/hatyaiwit.ac.th/innovative-center/หนาหลก"
-                class="nav-link px-2 text-body-secondary">Innovative Center</a></li>
-        <li class="nav-item"><a href="https://www.hatyaiwit.ac.th"
-                class="nav-link px-2 text-body-secondary">เว็ปโรงเรียน</a></li>
-        <li class="nav-item"><a href="http://202.129.48.202"
-                class="nav-link px-2 text-body-secondary">ระเบียนผลการเรียน</a></li>
-        <li class="nav-item"><a href="/about/webapp.html" class="nav-link px-2 text-body-secondary">About webapp</a>
-        </li>
-    </ul>
-    <!-- Stats Counter -->
-    <div align='center' style="display: none;"><a href='https://www.websitecounterfree.com'
-            style="color: #212529;text-decoration: none;">ยอดผู้เข้าชมทั้งหมด : <span><img style="margin-top: 10px;"
-                    width="200px" src='https://www.websitecounterfree.com/c.php?d=9&id=37132&s=4' border='0'
-                    alt='Free Website Counter'></span></a><br /></div>
+        <p class="col-md-4 mb-0 text-body-secondary">© 2022-2023 Siraphop Sukchu</p>
+        <a href="https://firebase.google.com"
+                class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+                <img style="margin-top : 10px"
+                        src="https://cdn.discordapp.com/attachments/1071401485239332864/1124285520776802395/logo-built_black-removebg-preview_1_2.png"
+                        class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="100" height="500"
+                        loading="lazy">
+        </a>
+        <ul class="nav col-md-4 justify-content-end">
+                <li class="nav-item"><a href="https://yorworplatform.com"
+                                class="nav-link px-2 text-body-secondary">Yorwor
+                                Platform</a></li>
+                <li class="nav-item"><a href="https://sites.google.com/hatyaiwit.ac.th/innovative-center/หนาหลก"
+                                class="nav-link px-2 text-body-secondary">Innovative Center</a></li>
+                <li class="nav-item"><a href="https://www.hatyaiwit.ac.th"
+                                class="nav-link px-2 text-body-secondary">เว็ปโรงเรียน</a></li>
+                <li class="nav-item"><a href="http://202.129.48.202"
+                                class="nav-link px-2 text-body-secondary">ระเบียนผลการเรียน</a></li>
+                <li class="nav-item"><a href="/about/webapp.html" class="nav-link px-2 text-body-secondary">About
+                                webapp</a>
+                </li>
+        </ul>
+        <!-- Stats Counter -->
+        <div align='center' style="display: none;"><a href='https://www.websitecounterfree.com'
+                        style="color: #212529;text-decoration: none;">ยอดผู้เข้าชมทั้งหมด : <span><img
+                                        style="margin-top: 10px;" width="200px"
+                                        src='https://www.websitecounterfree.com/c.php?d=9&id=37132&s=4' border='0'
+                                        alt='Free Website Counter'></span></a><br /></div>
 </footer>
